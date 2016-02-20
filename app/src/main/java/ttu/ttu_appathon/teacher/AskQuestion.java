34bd38c7587c24209fad4d34444b3cec83e7cc35
NilsG-S <nils.griswold.steiner@gmail.com>
2016-02-20 02:39:53 -0600

    Testing intents

--- app/src/main/java/ttu/ttu_appathon/teacher/AskQuestion.java
+++ app/src/main/java/ttu/ttu_appathon/teacher/AskQuestion.java
@@ -3,19 +3,13 @@ package ttu.ttu_appathon.teacher;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class CreateCourse extends AppCompatActivity {
-
+public class AskQuestion extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         Intent intent = getIntent();
-    }
-
-    public void createCourse(View view) {
-        Intent intent = new Intent(this, AskQuestion.class);
     }
 }
