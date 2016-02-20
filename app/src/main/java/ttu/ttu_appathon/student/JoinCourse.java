8b0feb93a5753a69d1a63a9d922c15e8607ed8ed
NilsG-S <nils.griswold.steiner@gmail.com>
2016-02-20 03:17:48 -0600

    Minor changes

--- app/src/main/java/ttu/ttu_appathon/student/JoinCourse.java
+++ app/src/main/java/ttu/ttu_appathon/student/JoinCourse.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import ttu.ttu_appathon.R;
+
 public class JoinCourse extends AppCompatActivity {
 
     @Override
