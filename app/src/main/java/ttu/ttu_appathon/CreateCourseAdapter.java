66fce24344cf6adb47d0d0a228b6a9e88768a587
NilsG-S <nils.griswold.steiner@gmail.com>
2016-02-20 00:25:01 -0600

    Basic adapter

---
+++ app/src/main/java/ttu/ttu_appathon/CreateCourseAdapter.java
@@ -0,0 +1,8 @@
+package ttu.ttu_appathon;
+
+/**
+ * Created by nilsg on 2/20/2016.
+ */
+public class CreateCourseAdapter {
+
+}
--- app/src/main/java/ttu/ttu_appathon/MainActivity.java
+++ app/src/main/java/ttu/ttu_appathon/MainActivity.java
@@ -2,18 +2,17 @@ package ttu.ttu_appathon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    // Testing, Testing
+    Button createCourse;
 
-
-    //second test
-
-
-    //hh
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        createCourse = (Button) findViewById(R.id.create_course);
     }
 }
