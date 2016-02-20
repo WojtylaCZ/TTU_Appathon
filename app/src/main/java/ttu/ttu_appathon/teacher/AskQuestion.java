188bd3e862d7e095a1a17f3b7d6f2de1a661cac7
NilsG-S <nils.griswold.steiner@gmail.com>
2016-02-20 08:49:58 -0600

    Formatting changes to ask_question

--- app/src/main/java/ttu/ttu_appathon/teacher/AskQuestion.java
+++ app/src/main/java/ttu/ttu_appathon/teacher/AskQuestion.java
@@ -3,10 +3,12 @@ package ttu.ttu_appathon.teacher;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import ttu.ttu_appathon.R;
 
 public class AskQuestion extends AppCompatActivity {
+    TextView displayID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +17,10 @@ public class AskQuestion extends AppCompatActivity {
         this.setContentView(R.layout.ask_question);
 
         Intent intent = getIntent();
+
+        displayID = (TextView) findViewById(R.id.display_ID);
+
+        // This is where we would change the TextView such that it displays the ID
+        // displayID.setText();
     }
 }
